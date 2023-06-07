b87359dae5e76dd5aee0b7461d9bfcf3e332db05
Kamalesh-Seervi <kkamalesh117@gmail.com>
2023-06-07 21:48:48 +0530

    autocomplete address part2

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,25 +1,30 @@
 import React from 'react'
 // import ReactDOM from 'react-dom';
 
-
+import MyMap, { Marker } from 'react-map-gl';
 import { AddressAutofill } from '@mapbox/search-js-react';
 
 export default function Search() {
-    
-    const addMarker = (lngLat) => {
-        new mapboxgl.Marker().setLngLat(lngLat).addTo(map);
-      };
-  
 
   return (
     <div>
 <form>
+<h7 style={{marginLeft: '11px', fontWeight:'bold'}}>Shipper Address</h7>
 <AddressAutofill accessToken="pk.eyJ1Ijoia2FteTAwNyIsImEiOiJjbGllZXllaGcwNHkyM2VzZXJmZjFiZWVmIn0.pwB-QPgLZ5nf2qtD2V19KQ">
 <input
-name="address" placeholder="Address" type="text" className="form-control" style={{ marginLeft: '10px' }}
+name="address"  title=" Your Address" placeholder="Address" type="text" className="form-control" style={{ marginLeft: '10px', marginTop:'10px' }}
 autoComplete="address-line1"
 />
 </AddressAutofill>
+
+<h7 style={{marginLeft: '11px', fontWeight:'bold'}}>Destination Address</h7>
+<AddressAutofill accessToken="pk.eyJ1Ijoia2FteTAwNyIsImEiOiJjbGllZXllaGcwNHkyM2VzZXJmZjFiZWVmIn0.pwB-QPgLZ5nf2qtD2V19KQ">
+<input
+name="address"  title=" Your Address" placeholder="Address" type="text" className="form-control" style={{ marginLeft: '10px', marginTop:'10px' }}
+autoComplete="address-line1"
+/>
+</AddressAutofill>
+
 
 
 </form>
